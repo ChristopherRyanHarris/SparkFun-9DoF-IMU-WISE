d2edb20b86a0af8d2aef658e85acef206646a3bf
RebeccaAnne91 <40864915+RebeccaAnne91@users.noreply.github.com>
2018-07-24 12:00:56 -0500

    Add files via upload

--- FES_Specific_Functions.ino
+++ FES_Specific_Functions.ino
@@ -56,17 +56,24 @@ void FES_Init ( CONTROL_TYPE *p_control, FES_TEST_TYPE *p_fes_state )
   /* Foot sensros are read from a voltage split */
   pinMode( FOOT_SENSOR_1_PIN, INPUT );
   pinMode( FOOT_SENSOR_2_PIN, INPUT );
+  pinMode( FOOT_SENSOR_3_PIN, INPUT );      //add 7-23-2018 Rebecca
+  pinMode( FOOT_SENSOR_4_PIN, INPUT );      //add 7-23-2018 Rebecca
   SET_FOOT_SENSOR_1_OUTPUT;
   SET_FOOT_SENSOR_2_OUTPUT;
+  SET_FOOT_SENSOR_3_OUTPUT;                 //add 7-23-2018 Rebecca
+  SET_FOOT_SENSOR_4_OUTPUT;                 //add 7-23-2018 Rebecca
   
-  p_fes_state->relays_on 						  = FALSE;
+  p_fes_state->relays_on 						   = FALSE;
   p_fes_state->relay_start_micros      = 0;
   p_fes_state->relay_start_iteration   = 0;
   p_fes_state->foot_sensor_1_val_ave   = 0;
   p_fes_state->foot_sensor_2_val_ave   = 0;
+  p_fes_state->foot_sensor_3_val_ave   = 0;   //add 7-23-2018 Rebecca
+  p_fes_state->foot_sensor_4_val_ave   = 0;   //add 7-23-2018 Rebecca
   p_fes_state->foot_sensor_1_volts_ave = 0;
   p_fes_state->foot_sensor_2_volts_ave = 0;
-  
+  p_fes_state->foot_sensor_3_volts_ave = 0;   //add 7-23-2018 Rebecca
+  p_fes_state->foot_sensor_4_volts_ave = 0;   //add 7-23-2018 Rebecca
 }
 
 /*****************************************************************
@@ -90,25 +97,34 @@ void FES_Update (
   /* Read foot switch values */
   p_fes_state->foot_sensor_1_val       = FOOT_SENSOR_1_VAL;
   p_fes_state->foot_sensor_2_val       = FOOT_SENSOR_2_VAL;
+  p_fes_state->foot_sensor_3_val       = FOOT_SENSOR_3_VAL;   //add 7-23-2018 Rebecca
+  p_fes_state->foot_sensor_4_val       = FOOT_SENSOR_4_VAL;   //add 7-23-2018 Rebecca
   p_fes_state->foot_sensor_1_val_ave   = Windowed_Mean( p_fes_state->foot_sensor_1_val_ave, (float)p_fes_state->foot_sensor_1_val, p_control->SampleNumber, FOOT_SENSOR_1_VAL_ALPHA );
   p_fes_state->foot_sensor_2_val_ave   = Windowed_Mean( p_fes_state->foot_sensor_2_val_ave, (float)p_fes_state->foot_sensor_2_val, p_control->SampleNumber, FOOT_SENSOR_2_VAL_ALPHA );
+  p_fes_state->foot_sensor_3_val_ave   = Windowed_Mean( p_fes_state->foot_sensor_3_val_ave, (float)p_fes_state->foot_sensor_3_val, p_control->SampleNumber, FOOT_SENSOR_3_VAL_ALPHA );    //add 7-23-2018 Rebecca
+  p_fes_state->foot_sensor_4_val_ave   = Windowed_Mean( p_fes_state->foot_sensor_4_val_ave, (float)p_fes_state->foot_sensor_4_val, p_control->SampleNumber, FOOT_SENSOR_4_VAL_ALPHA );    //add 7-23-2018 Rebecca
   p_fes_state->foot_sensor_1_volts     = FOOT_SENSOR_1_VOLTS;
   p_fes_state->foot_sensor_2_volts     = FOOT_SENSOR_2_VOLTS;
+  p_fes_state->foot_sensor_3_volts     = FOOT_SENSOR_3_VOLTS;    //add 7-23-2018 Rebecca
+  p_fes_state->foot_sensor_4_volts     = FOOT_SENSOR_4_VOLTS;    //add 7-23-2018 Rebecca
   p_fes_state->foot_sensor_1_volts_ave = Windowed_Mean( p_fes_state->foot_sensor_1_volts_ave, p_fes_state->foot_sensor_1_volts, p_control->SampleNumber, FOOT_SENSOR_1_VOLTS_ALPHA );
   p_fes_state->foot_sensor_2_volts_ave = Windowed_Mean( p_fes_state->foot_sensor_2_volts_ave, p_fes_state->foot_sensor_2_volts, p_control->SampleNumber, FOOT_SENSOR_2_VOLTS_ALPHA );
+  p_fes_state->foot_sensor_3_volts_ave = Windowed_Mean( p_fes_state->foot_sensor_3_volts_ave, p_fes_state->foot_sensor_3_volts, p_control->SampleNumber, FOOT_SENSOR_3_VOLTS_ALPHA );    //add 7-23-2018 Rebecca
+  p_fes_state->foot_sensor_4_volts_ave = Windowed_Mean( p_fes_state->foot_sensor_4_volts_ave, p_fes_state->foot_sensor_4_volts, p_control->SampleNumber, FOOT_SENSOR_4_VOLTS_ALPHA );    //add 7-23-2018 Rebecca
+  
   /* At given event, trigger relay */
 	if( PHASE_ANGLE_SWITCH_ON_EVENT )
 	{
 		/* Relay : On (High), update state variables */
 		if( p_fes_state->relays_on==FALSE )
 		{
-		  LOG_INFO( "SWITCH RELAY ON");
+		  LOG_INFO( "SWITCH RELAY 1 ON");
 			/* Update current relay state */
 			p_fes_state->relays_on = TRUE;
 			
 			/* Set both relays on */
 			RELAY_1_SET_HIGH;
-			RELAY_2_SET_HIGH;
+			RELAY_2_SET_LOW;
 			
 			/* Record relay on start time variable */
 			p_fes_state->relay_start_micros = micros();
@@ -120,6 +136,29 @@ void FES_Update (
 		/* Update iterations up counter */
 		p_fes_state->relay_on_iterations = p_fes_state->relay_start_iteration - p_control->SampleNumber;
 	}
+  else if( PHASE_ANGLE_SWITCH_ON_EVENT_DF )
+  {
+   /* Relay : On (High), update state variables */
+     if( p_fes_state->relays_on==FALSE )
+     {
+       LOG_INFO( "SWITCH RELAY 2 ON");
+       /* Update current relay state */
+       p_fes_state->relays_on = TRUE;
+       
+       /* Set both relays on */
+       RELAY_1_SET_LOW;
+       RELAY_2_SET_HIGH;
+       
+       /* Record relay on start time variable */
+       p_fes_state->relay_start_micros = micros();
+       /* Record relay on start iteration variable */
+       p_fes_state->relay_start_iteration = p_control->SampleNumber;
+     }
+     /* Update time up counter */
+     p_fes_state->relay_on_micros     = p_fes_state->relay_start_micros - micros();
+     /* Update iterations up counter */
+     p_fes_state->relay_on_iterations = p_fes_state->relay_start_iteration - p_control->SampleNumber;
+  }
 	else
 	{
 		/* Relay : Off (Low), reset state variables */
