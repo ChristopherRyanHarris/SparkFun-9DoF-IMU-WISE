d354a7cd676748fb05d63ff1130112baa8a9b5bd
ChristopherRyanHarris <crh150430@utdallas.edu>
2019-08-20 13:34:41 -0500

    Cleanup for FES testing
    Removed redundant file (used newer)
    Switched logging to SD off
    fixed platform id typo in logging_functinos.ino

--- FES_Specific_Functions.ino
+++ FES_Specific_Functions.ino
@@ -44,36 +44,39 @@ void FES_Init ( CONTROL_TYPE *p_control, FES_TEST_TYPE *p_fes_state )
   ** Initialize low to set relay closed */
   
   SET_RELAY_1_OUTPUT;
-	RELAY_1_SET_LOW;
+  RELAY_1_SET_LOW;
   //pinMode( RELAY_1_PIN, OUTPUT );
   //digitalWrite( RELAY_1_PIN,LOW );
-	
+  
   SET_RELAY_2_OUTPUT;
-	RELAY_2_SET_LOW;
+  RELAY_2_SET_LOW;
   //pinMode( RELAY_1_PIN, OUTPUT );
   //digitalWrite( RELAY_1_PIN,LOW );
   
   /* Foot sensros are read from a voltage split */
   pinMode( FOOT_SENSOR_1_PIN, INPUT );
   pinMode( FOOT_SENSOR_2_PIN, INPUT );
-  pinMode( FOOT_SENSOR_3_PIN, INPUT );      //add 7-23-2018 Rebecca
-  pinMode( FOOT_SENSOR_4_PIN, INPUT );      //add 7-23-2018 Rebecca
+  pinMode( FOOT_SENSOR_3_PIN, INPUT );
+  pinMode( FOOT_SENSOR_4_PIN, INPUT );
   SET_FOOT_SENSOR_1_OUTPUT;
   SET_FOOT_SENSOR_2_OUTPUT;
-  SET_FOOT_SENSOR_3_OUTPUT;                 //add 7-23-2018 Rebecca
-  SET_FOOT_SENSOR_4_OUTPUT;                 //add 7-23-2018 Rebecca
+  SET_FOOT_SENSOR_3_OUTPUT;
+  SET_FOOT_SENSOR_4_OUTPUT;
   
-  p_fes_state->relays_on 						   = FALSE;
-  p_fes_state->relay_start_micros      = 0;
-  p_fes_state->relay_start_iteration   = 0;
+  p_fes_state->relay1_on               = FALSE;
+  p_fes_state->relay2_on               = FALSE;
+  p_fes_state->relay1_start_micros     = 0;
+  p_fes_state->relay2_start_micros     = 0;
+  p_fes_state->relay1_start_iteration  = 0;
+  p_fes_state->relay2_start_iteration  = 0;
   p_fes_state->foot_sensor_1_val_ave   = 0;
   p_fes_state->foot_sensor_2_val_ave   = 0;
-  p_fes_state->foot_sensor_3_val_ave   = 0;   //add 7-23-2018 Rebecca
-  p_fes_state->foot_sensor_4_val_ave   = 0;   //add 7-23-2018 Rebecca
+  p_fes_state->foot_sensor_3_val_ave   = 0;
+  p_fes_state->foot_sensor_4_val_ave   = 0;
   p_fes_state->foot_sensor_1_volts_ave = 0;
   p_fes_state->foot_sensor_2_volts_ave = 0;
-  p_fes_state->foot_sensor_3_volts_ave = 0;   //add 7-23-2018 Rebecca
-  p_fes_state->foot_sensor_4_volts_ave = 0;   //add 7-23-2018 Rebecca
+  p_fes_state->foot_sensor_3_volts_ave = 0;
+  p_fes_state->foot_sensor_4_volts_ave = 0;
 }
 
 /*****************************************************************
@@ -86,6 +89,7 @@ void FES_Init ( CONTROL_TYPE *p_control, FES_TEST_TYPE *p_fes_state )
 ** DESCRIPTION:
 **    This function updates the FES specific variables
 */
+
 void FES_Update ( 
   CONTROL_TYPE        *p_control, 
   FES_TEST_TYPE       *p_fes_state,
@@ -97,108 +101,141 @@ void FES_Update (
   /* Read foot switch values */
   p_fes_state->foot_sensor_1_val       = FOOT_SENSOR_1_VAL;
   p_fes_state->foot_sensor_2_val       = FOOT_SENSOR_2_VAL;
-  p_fes_state->foot_sensor_3_val       = FOOT_SENSOR_3_VAL;   //add 7-23-2018 Rebecca
-  p_fes_state->foot_sensor_4_val       = FOOT_SENSOR_4_VAL;   //add 7-23-2018 Rebecca
+  p_fes_state->foot_sensor_3_val       = FOOT_SENSOR_3_VAL;
+  p_fes_state->foot_sensor_4_val       = FOOT_SENSOR_4_VAL;
   p_fes_state->foot_sensor_1_val_ave   = Windowed_Mean( p_fes_state->foot_sensor_1_val_ave, (float)p_fes_state->foot_sensor_1_val, p_control->SampleNumber, FOOT_SENSOR_1_VAL_ALPHA );
   p_fes_state->foot_sensor_2_val_ave   = Windowed_Mean( p_fes_state->foot_sensor_2_val_ave, (float)p_fes_state->foot_sensor_2_val, p_control->SampleNumber, FOOT_SENSOR_2_VAL_ALPHA );
-  p_fes_state->foot_sensor_3_val_ave   = Windowed_Mean( p_fes_state->foot_sensor_3_val_ave, (float)p_fes_state->foot_sensor_3_val, p_control->SampleNumber, FOOT_SENSOR_3_VAL_ALPHA );    //add 7-23-2018 Rebecca
-  p_fes_state->foot_sensor_4_val_ave   = Windowed_Mean( p_fes_state->foot_sensor_4_val_ave, (float)p_fes_state->foot_sensor_4_val, p_control->SampleNumber, FOOT_SENSOR_4_VAL_ALPHA );    //add 7-23-2018 Rebecca
+  p_fes_state->foot_sensor_3_val_ave   = Windowed_Mean( p_fes_state->foot_sensor_3_val_ave, (float)p_fes_state->foot_sensor_3_val, p_control->SampleNumber, FOOT_SENSOR_3_VAL_ALPHA );
+  p_fes_state->foot_sensor_4_val_ave   = Windowed_Mean( p_fes_state->foot_sensor_4_val_ave, (float)p_fes_state->foot_sensor_4_val, p_control->SampleNumber, FOOT_SENSOR_4_VAL_ALPHA );
   p_fes_state->foot_sensor_1_volts     = FOOT_SENSOR_1_VOLTS;
   p_fes_state->foot_sensor_2_volts     = FOOT_SENSOR_2_VOLTS;
-  p_fes_state->foot_sensor_3_volts     = FOOT_SENSOR_3_VOLTS;    //add 7-23-2018 Rebecca
-  p_fes_state->foot_sensor_4_volts     = FOOT_SENSOR_4_VOLTS;    //add 7-23-2018 Rebecca
+  p_fes_state->foot_sensor_3_volts     = FOOT_SENSOR_3_VOLTS;
+  p_fes_state->foot_sensor_4_volts     = FOOT_SENSOR_4_VOLTS;
   p_fes_state->foot_sensor_1_volts_ave = Windowed_Mean( p_fes_state->foot_sensor_1_volts_ave, p_fes_state->foot_sensor_1_volts, p_control->SampleNumber, FOOT_SENSOR_1_VOLTS_ALPHA );
   p_fes_state->foot_sensor_2_volts_ave = Windowed_Mean( p_fes_state->foot_sensor_2_volts_ave, p_fes_state->foot_sensor_2_volts, p_control->SampleNumber, FOOT_SENSOR_2_VOLTS_ALPHA );
-  p_fes_state->foot_sensor_3_volts_ave = Windowed_Mean( p_fes_state->foot_sensor_3_volts_ave, p_fes_state->foot_sensor_3_volts, p_control->SampleNumber, FOOT_SENSOR_3_VOLTS_ALPHA );    //add 7-23-2018 Rebecca
-  p_fes_state->foot_sensor_4_volts_ave = Windowed_Mean( p_fes_state->foot_sensor_4_volts_ave, p_fes_state->foot_sensor_4_volts, p_control->SampleNumber, FOOT_SENSOR_4_VOLTS_ALPHA );    //add 7-23-2018 Rebecca
-
-  /* If FootSwitch in Contact, Turn on LED */
-  if( p_fes_state->foot_sensor_1_volts_ave > 1 )
-    LED_DF_SET_HIGH;
-    else if ( p_fes_state->foot_sensor_1_volts_ave < 1 )
-    LED_DF_SET_LOW;
-    else if ( p_fes_state->foot_sensor_2_volts_ave > 1 )
-    LED_PF_SET_HIGH;
-    else if ( p_fes_state->foot_sensor_2_volts_ave < 1 )
-    LED_PF_SET_LOW;
-    else
-    LED_DF_SET_LOW;
-    LED_PF_SET_LOW;
-   
+  p_fes_state->foot_sensor_3_volts_ave = Windowed_Mean( p_fes_state->foot_sensor_3_volts_ave, p_fes_state->foot_sensor_3_volts, p_control->SampleNumber, FOOT_SENSOR_3_VOLTS_ALPHA );
+  p_fes_state->foot_sensor_4_volts_ave = Windowed_Mean( p_fes_state->foot_sensor_4_volts_ave, p_fes_state->foot_sensor_4_volts, p_control->SampleNumber, FOOT_SENSOR_4_VOLTS_ALPHA );
+/* ********************************************************************************************************* */
+/* If FootSwitch in Contact, Turn on LED Monitor */
+/* (unused) If added, un-comment Foot Sensor LED Monitor definitions in "FES_Specific_Config.h" */
+/*  if( LED_H_ON )
+  {
+    LED_H_SET_HIGH;
+  }
+  else if( LED_T_ON )
+  {
+    LED_T_SET_HIGH;
+  }
+  else
+  {
+    LED_H_SET_LOW;
+    LED_T_SET_LOW;
+  }
+*/
+/* ********************************************************************************************************* */
   /* At given event, trigger relay */
-	if( PHASE_ANGLE_SWITCH_ON_EVENT )
-	{
-		/* Relay : On (High), update state variables */
-		if( p_fes_state->relays_on==FALSE )
-		{
-		  LOG_INFO( "SWITCH RELAY 1 ON");
-			/* Update current relay state */
-			p_fes_state->relays_on = TRUE;
-			
-			/* Set both relays on */
-			RELAY_1_SET_HIGH;
-			RELAY_2_SET_LOW;
-			
-			/* Record relay on start time variable */
-			p_fes_state->relay_start_micros = micros();
-			/* Record relay on start iteration variable */
-			p_fes_state->relay_start_iteration = p_control->SampleNumber;
-		}
-		/* Update time up counter */
-		p_fes_state->relay_on_micros     = p_fes_state->relay_start_micros - micros();
-		/* Update iterations up counter */
-		p_fes_state->relay_on_iterations = p_fes_state->relay_start_iteration - p_control->SampleNumber;
-	}
+  if( NO_STIM )
+  {
+     LOG_INFO( "SWITCH RELAYS OFF");
+     /* Update current relay state */
+     p_fes_state->relay1_on = FALSE;
+     p_fes_state->relay2_on = FALSE;
+     /* Set both relays on */
+     RELAY_1_SET_LOW;
+     RELAY_2_SET_LOW;
+
+     /* Reset recorded start time variable */
+     p_fes_state->relay1_start_micros    = -1;
+     p_fes_state->relay2_start_micros    = -1;
+     /* Reset recorded start iteration variable */
+     p_fes_state->relay1_start_iteration = -1;
+     p_fes_state->relay2_start_iteration = -1;
+     
+     /* Reset time up counter */
+     p_fes_state->relay1_on_micros       = -1;
+     p_fes_state->relay2_on_micros       = -1;
+     /* Reset iterations up counter */
+     p_fes_state->relay1_on_iterations   = -1;
+     p_fes_state->relay2_on_iterations   = -1;
+  }
+  else if( PHASE_ANGLE_SWITCH_ON_EVENT )
+  {
+    /* Relay : On (High), update state variables */
+    //if( p_fes_state->relay1_on==FALSE )
+    //{
+      LOG_INFO( "SWITCH RELAY 1 ON");
+      /*  Update current relay state */
+      p_fes_state->relay1_on = TRUE;
+      p_fes_state->relay2_on = FALSE;
+      /* Set both relays on */
+      RELAY_1_SET_HIGH;
+      RELAY_2_SET_LOW;
+      
+      /* Record relay on start time variable */
+      p_fes_state->relay1_start_micros = micros();
+      /* Record relay on start iteration variable */
+      p_fes_state->relay1_start_iteration = p_control->SampleNumber;
+    //}
+    /* Update time up counter */
+    p_fes_state->relay1_on_micros     = p_fes_state->relay1_start_micros - micros();
+    /* Update iterations up counter */
+    p_fes_state->relay1_on_iterations = p_fes_state->relay1_start_iteration - p_control->SampleNumber;
+  }
   else if( PHASE_ANGLE_SWITCH_ON_EVENT_DF )
   {
    /* Relay : On (High), update state variables */
-     if( p_fes_state->relays_on==FALSE )
-     {
+     //if( p_fes_state->relay2_on==FALSE )
+     //{
        LOG_INFO( "SWITCH RELAY 2 ON");
        /* Update current relay state */
-       p_fes_state->relays_on = TRUE;
-       
+       p_fes_state->relay1_on = FALSE;
+       p_fes_state->relay2_on = TRUE;
        /* Set both relays on */
        RELAY_1_SET_LOW;
        RELAY_2_SET_HIGH;
        
        /* Record relay on start time variable */
-       p_fes_state->relay_start_micros = micros();
+       p_fes_state->relay2_start_micros = micros();
        /* Record relay on start iteration variable */
-       p_fes_state->relay_start_iteration = p_control->SampleNumber;
-     }
+       p_fes_state->relay2_start_iteration = p_control->SampleNumber;
+     //}
      /* Update time up counter */
-     p_fes_state->relay_on_micros     = p_fes_state->relay_start_micros - micros();
+     p_fes_state->relay2_on_micros     = p_fes_state->relay2_start_micros - micros();
      /* Update iterations up counter */
-     p_fes_state->relay_on_iterations = p_fes_state->relay_start_iteration - p_control->SampleNumber;
+     p_fes_state->relay2_on_iterations = p_fes_state->relay2_start_iteration - p_control->SampleNumber;
   }
-	else
-	{
-		/* Relay : Off (Low), reset state variables */
-		if( p_fes_state->relays_on==TRUE )
-		{
-		  LOG_INFO( "SWITCH RELAY OFF");
-			/* Update current relay state */
-			p_fes_state->relays_on = FALSE;
-			
-			/* Set both relays off */
-			RELAY_1_SET_LOW;
-			RELAY_2_SET_LOW;
-			
-			/* NOTE:
-			** By maxing the counter, we ensure we only 
-			** trigger the relay on at the desired moment 
-			** See the header file for more info */
-			
-			/* Reset recorded start time variable */
-			p_fes_state->relay_start_micros    = -1;
-			/* Reset recorded start iteration variable */
-			p_fes_state->relay_start_iteration = -1;
-			
-			/* Reset time up counter */
-			p_fes_state->relay_on_micros       = -1;
-			/* Reset iterations up counter */
-			p_fes_state->relay_on_iterations   = -1;
-		}
-	}
+  else
+  {
+    /* Relay : Off (Low), reset state variables */
+    if( (p_fes_state->relay1_on)==TRUE||(p_fes_state->relay2_on)==TRUE )
+    {
+      LOG_INFO( "SWITCH RELAYS OFF");
+      /* Update current relay state */
+      p_fes_state->relay1_on = FALSE;
+      p_fes_state->relay2_on = FALSE;
+      /* Set both relays off */
+      RELAY_1_SET_LOW;
+      RELAY_2_SET_LOW;
+      
+      /* NOTE:
+      ** By maxing the counter, we ensure we only 
+      ** trigger the relay on at the desired moment 
+      ** See the header file for more info */
+      
+      /* Reset recorded start time variable */
+      p_fes_state->relay1_start_micros    = -1;
+      p_fes_state->relay2_start_micros    = -1;
+      /* Reset recorded start iteration variable */
+      p_fes_state->relay1_start_iteration = -1;
+      p_fes_state->relay2_start_iteration = -1;
+      
+      /* Reset time up counter */
+      p_fes_state->relay1_on_micros       = -1;
+      p_fes_state->relay2_on_micros       = -1;
+      /* Reset iterations up counter */
+      p_fes_state->relay1_on_iterations   = -1;
+      p_fes_state->relay2_on_iterations   = -1;
+    }
+  }
 } /* End FES_Update() */
+
